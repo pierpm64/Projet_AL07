84a9957ed7bdcf81536eec931150b93ad2ea42e1
Pierre PM <pierpm@gmail.com>
2021-07-16 07:56:45 +0200

    cleanup prod version

--- Node.js/API_Transport_Nantes/test_api_with_cloud_Rect_MongoDB.bat
+++
@@ -1,4 +0,0 @@
-set MongoUri=mongodb+srv://pierpm:Midoempi@AL07@cluster0.zpmhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-set MongoDb=
-node ./test_api_tan.js
-pause
--- Node.js/API_Transport_Nantes/test_api_with_cloud_prod_MongoDB.bat
+++
@@ -1,5 +0,0 @@
-set MongoUri=mongodb+srv://pierpm:Midoempi@AL07@cluster0.ehndb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-set mongouri
-set MongoDb=
-node ./test_api_tan.js day
-pause
